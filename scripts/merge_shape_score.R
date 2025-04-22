33f1d5a490ba8b06af168249dc45bed4c393a401
JohnnyChen-slu <junhao.chen.1@slu.edu>
2025-04-22 15:26:44 -0500

    add dominate option

--- scripts/merge_shape_score.R
+++ scripts/merge_shape_score.R
@@ -2,6 +2,9 @@
 suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(optparse))
 
+# Version number
+VERSION <- "0.2.0"
+
 # Define command line options
 option_list <- list(
   make_option(c("-a", "--assigned"), type="character", default=NULL,
@@ -9,7 +12,11 @@ option_list <- list(
   make_option(c("-s", "--shape"), type="character", default=NULL,
               help="Path to shape file", metavar="character"),
   make_option(c("-o", "--output"), type="character", default=NULL,
-              help="Path to output file [default: assignedClusters prefix with _merged.txt]")
+              help="Path to output file [default: assignedClusters prefix with _merged.txt]"),
+  make_option(c("-d", "--dominate"), action="store_true", default=FALSE,
+              help="Output only the dominant cluster for each gene based on maximum tags value"),
+  make_option(c("-v", "--version"), action="store_true", default=FALSE,
+              help="Print version number and exit")
 )
 
 # Parse command line arguments
@@ -19,6 +26,12 @@ opt <- parse_args(opt_parser, print_help_and_exit=FALSE)
 # If no arguments provided, print help and exit
 if (length(commandArgs(trailingOnly=TRUE)) == 0) {
   print_help(opt_parser)
+  quit(status=0)
+}
+
+# Print version if requested
+if (opt$version) {
+  cat("merge_shape_score.R version", VERSION, "\n")
   quit(status=0)
 }
 
@@ -40,6 +53,14 @@ table2 <- read_tsv(opt$shape)
 # Merge tables based on cluster column
 merged_table <- table1 %>%
   left_join(select(table2, cluster, shape.score), by = "cluster")
+
+# If dominate option is used, filter for dominant cluster per gene
+if (opt$dominate) {
+  merged_table <- merged_table %>%
+    group_by(gene) %>%
+    slice(which.max(tags)) %>%
+    ungroup()
+}
 
 # Write the output
 write_tsv(merged_table, opt$output)
